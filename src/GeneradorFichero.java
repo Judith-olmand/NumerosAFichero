407b182c78ce78fd0d5f7e09b2ce2ea22f11dc9c
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-06 19:06:40 +0100

    acabado

--- src/GeneradorFichero.java
+++ src/GeneradorFichero.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 public class GeneradorFichero {
+    public static void generadorFichero(List<String> lineas) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("salida.txt"))){
+
+            for(String linea : lineas){
+                writer.write(linea);
+                writer.newLine();
+            }
+            System.out.println("Fichero 'salida.txt' generado correctamente");
+
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al escribir en el archivo: " +
+                    e.getMessage());
+        }
+    }
 }
--- src/GenerarLineas.java
+++ src/GenerarLineas.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenerarLineas {
+    public static List<String> generarLineas(int numero){
+        List<String> lineas = new ArrayList<>();
+
+        //i --> indica la línea
+        for(int i = 1; i <= numero; i++){
+            //Crea un constructor de String
+            StringBuilder linea = new StringBuilder();
+
+            //j --> indica los numeros a escribir en la linea
+            for(int j = 1; j <= i; j++){
+                //append "pega" detras de lo que haya
+                linea.append(j).append(" ");
+            }
+
+            //Convierte a texto y lo almacena en la List
+            lineas.add(linea.toString());
+        }
+        return lineas;
+    }
 }
